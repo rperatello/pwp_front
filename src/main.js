68c5cd0929a35c148b9dde629728ae6e482e4953
Roger Peratello <roger.peratello@hotmail.com>
2022-09-07 12:53:13 -0300

    create pwa

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './config/router'
 import store from './config/store'
-import "./registerServiceWorker";
+import "./registerServiceWorker"
 
 import './config/bootstrap'
 import './config/msgs'
